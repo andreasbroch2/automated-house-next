22496e0b0ca0ec9412aaa9af6d8e67319ea4b1f2
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-04-14 23:38:13 +0200

    Update post.tsx

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -5,17 +5,14 @@ const Post = ({ post }) => {
 	return (
 		<div className="mb-8 relative">
 			<Link href={`/blog/${post?.slug}/`}>
-			<figure className="overflow-hidden relative h-64">
-				<Image className='blog-box-image relative' src={post?.featuredImage.node.sourceUrl} alt={post?.featuredImage.node.altText} fill title={post?.title ?? ''} sizes='100vw' />
-			</figure>
-			<div className='textbox'>
-				
+				<figure className="overflow-hidden relative h-64">
+					<Image className='blog-box-image relative' src={post?.featuredImage.node.sourceUrl} alt={post?.featuredImage.node.altText} fill title={post?.title ?? ''} sizes='100vw' />
+				</figure>
+				<div className='textbox'>
 					<h2 className="font-bold mb-3 text-lg hover:text-blue-500" dangerouslySetInnerHTML={{ __html: post?.title ?? '' }} />
-				
-				<div dangerouslySetInnerHTML={{ __html: post?.excerpt ?? '' }} />
-			</div>
+					<div dangerouslySetInnerHTML={{ __html: post?.excerpt ?? '' }} />
+				</div>
 			</Link>
-
 		</div>
 	);
 };
