fe44117da0c578a7f9aba5509130cd8d7622ff07
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-04-14 19:20:11 +0200

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://dksmarthome.dk',
+    siteUrl: 'https://myautomatedhouse.com',
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
